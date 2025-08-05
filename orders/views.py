35b6a4765961e0e9f0d779188510fac21f32f752
Priyanshi43 <priyanshi315tripathi@gmail.com>
2025-08-05 14:13:29 +0530

    17928

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,12 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def menu_home(request):
+    api_url = 'http://localhost:800/api/menu/'
+    try:
+        response = request.get(api_url)
+        menu_items = response = response.json()
+    except:
+        menu_items = []
+
+    return render(request,'orders/menu_home.html',{'menu_items':menu_items})        
