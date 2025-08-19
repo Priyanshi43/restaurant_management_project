1ab1852aad7924fe026e1772fefac7d178164566
Priyanshi43 <priyanshi315tripathi@gmail.com>
2025-08-19 16:34:06 +0530

    35837

--- home/views.py
+++ home/views.py
@@ -4,4 +4,7 @@ def about(request):
     return render(request,'about.html')
 
 def homepage_view(request):
+    context = {
+        "phone": settings.RESTAURANT_PHONE   
+    }
     return render(request, 'home.html')    
