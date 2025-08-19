e7c9c7cd14f55aa84159dae0b91bba4182734318
Priyanshi43 <priyanshi315tripathi@gmail.com>
2025-08-19 17:02:55 +0530

    35834

--- home/views.py
+++ home/views.py
@@ -8,3 +8,12 @@ def homepage_view(request):
         "phone": settings.RESTAURANT_PHONE   
     }
     return render(request, 'home.html')    
+
+def menu(request):
+    menu_items = [
+        {"name": "Pizza","Price": "200"},
+        {"name": "Burger", "Price": "100"},
+        {"name": "Pasta", "Price": "150"},
+        {"name": "Coffee", "Price": "80"},
+    ]    
+    return render(request, "menu.html", {"menu_items": menu_items})
